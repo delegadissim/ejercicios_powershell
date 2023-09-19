9bf11db67b6844955fcfd7bc87659412ef40ee3c
delegadissim <alumas9513@ieselcaminas.org>
2023-09-19 13:39:47 +0200

    Cambiado el p3/ej6 para que liste los directorios de forma recursiva

--- practica3/ej6.ps1
+++ practica3/ej6.ps1
@@ -4,7 +4,7 @@ $ruta = Read-Host "Escribe la ruta al archivo/directorio"
 
 if (Test-Path -PathType Container $ruta){
     Write-Output "La ruta existe y es un directorio"
-    Get-ChildItem -Path $ruta
+    Get-ChildItem -Recurse $ruta
 }
 elseif (Test-Path -Path $ruta){
     Write-Output "La ruta existe pero no es un directorio"
