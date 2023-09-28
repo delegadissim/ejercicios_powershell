3b21388cc60ca674165187aecf7a361a4ca531ca
delegadissim <alumas9513@ieselcaminas.org>
2023-09-28 08:47:12 +0200

    ej_test

---
+++ practica4/ej_test.ps1
@@ -0,0 +1,7 @@
+﻿#exercici 7.5
+Clear-Host
+
+
+[int]$nombre = Read-Host "Quina altura vols al triangle?"
+
+for ( $i=1; $i -le $nombre; $i++ ){    Write-Host ""    for ( $j=$i;$j -ge 1; $j-- ){        write-host -NoNewline "$j "    }}
