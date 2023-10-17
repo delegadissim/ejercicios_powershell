22e0fb52d5b08b36a5b34fa48acdc1e80f0ddec6
delegadissim <alumas9513@ieselcaminas.org>
2023-10-17 13:10:28 +0200

    millorada practica 5

--- Tema 2/practica 5/UT2_P5.ps1
+++ Tema 2/practica 5/UT2_P5.ps1
@@ -13,14 +13,14 @@ New-Item -Path $empresa -ItemType Container
 
 
 
-#Creem la subestructura
+#Creem la subestructura, compartim les carpetes i donem els permisos que calen als arxius compartits
 $departaments = Import-Csv C:\Users\Administrador\Desktop\departamentos.csv -Delimiter ";" 
 foreach ($dep in $departaments){
     Remove-SmbShare -Name $($dep.departamento) -Force
     New-Item -Path "$empresa$($dep.departamento)" -ItemType Container
-    New-SmbShare -Path $empresa$($dep.departamento) -Name $($dep.departamento) 
-    Grant-SmbShareAccess -Name $($dep.departamento) -AccountName $($dep.departamento) -AccessRight Change -Force
-    Grant-SmbShareAccess -Name $($dep.departamento) -AccountName Administradores -AccessRight Full -Force
-    Grant-SmbShareAccess -Name $($dep.departamento) -AccountName "Usuarios del dominio" -AccessRight Read -Force
+    New-SmbShare -Path $empresa$($dep.departamento) -Name $($dep.departamento) -FullAccess Administradores -ChangeAccess $($dep.departamento) -ReadAccess "Usuarios del dominio"
+    #Grant-SmbShareAccess -Name $($dep.departamento) -AccountName $($dep.departamento) -AccessRight Change -Force
+    #Grant-SmbShareAccess -Name $($dep.departamento) -AccountName Administradores -AccessRight Full -Force
+    #Grant-SmbShareAccess -Name $($dep.departamento) -AccountName "Usuarios del dominio" -AccessRight Read -Force
     Revoke-SmbShareAccess -Name $($dep.departamento) -AccountName todos -Force
 }
