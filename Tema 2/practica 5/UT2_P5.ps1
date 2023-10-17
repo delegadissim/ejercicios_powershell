a28bd7c995347b6aad34d0c5faf4647022eea76f
delegadissim <alumas9513@ieselcaminas.org>
2023-10-17 12:58:26 +0200

    script practica 5

---
+++ Tema 2/practica 5/UT2_P5.ps1
@@ -0,0 +1,26 @@
+#Script per fer la practica 3
+$empresa="C:\Empresa\"
+clear-host
+
+#comprovem si existem la carpeta Empresa
+if ( Test-Path -Path $empresa ){
+    Remove-Item -Recurse $empresa -Force
+    # Remove-SmbShare -Name Empresa -Force
+}
+
+#creem la carpeta empresa i la compartim
+New-Item -Path $empresa -ItemType Container
+
+
+
+#Creem la subestructura
+$departaments = Import-Csv C:\Users\Administrador\Desktop\departamentos.csv -Delimiter ";" 
+foreach ($dep in $departaments){
+    Remove-SmbShare -Name $($dep.departamento) -Force
+    New-Item -Path "$empresa$($dep.departamento)" -ItemType Container
+    New-SmbShare -Path $empresa$($dep.departamento) -Name $($dep.departamento) 
+    Grant-SmbShareAccess -Name $($dep.departamento) -AccountName $($dep.departamento) -AccessRight Change -Force
+    Grant-SmbShareAccess -Name $($dep.departamento) -AccountName Administradores -AccessRight Full -Force
+    Grant-SmbShareAccess -Name $($dep.departamento) -AccountName "Usuarios del dominio" -AccessRight Read -Force
+    Revoke-SmbShareAccess -Name $($dep.departamento) -AccountName todos -Force
+}
